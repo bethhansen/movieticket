b486816c3b10e678d3de60e55d4f73b4ff4e7904
Beth Hansen, Evan Johnson and Haneen AbuKhater <beth+evan+haneen@>
2017-07-18 13:19:08 -0700

    add comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,12 @@
 //Business Logic
-function MovieSpec (title, time, age, number) {
-  this.feature = title;
-  this.time = time;
-  this.age = age;
-  this.quantity = number;
+function MovieSpec (title, time, age, number) { //contstructor blueprint
+  this.feature = title; //assigning the past value into the object parameter
+  this.time = time; //assigning the past value into the object parameter
+  this.age = age; //assigning the past value into the object parameter
+  this.quantity = number; //assigning the past value into the object parameter
 }
 
-MovieSpec.prototype.movieTitle = function () {
+MovieSpec.prototype.movieTitle = function () { //prototype toolbox to store methods for objects to utilize later
   if (this.time === 10) {
     return "Evening ";
   } else if (this.time === 8){
@@ -16,7 +16,7 @@ MovieSpec.prototype.movieTitle = function () {
   }
 }
 
-MovieSpec.prototype.price = function() {
+MovieSpec.prototype.price = function() { //prototype toolbox to store methods for objects to utilize later
   return (this.time + this.age) * this.quantity;
 }
 
@@ -25,19 +25,19 @@ $(document).ready(function() {
   $('#movies').submit(function(event) {
     event.preventDefault();
 
-    var featureTitle = $('#features').val();
+    var featureTitle = $('#features').val(); //create variable that gives a value to our feature show
 
-    var movieTime = parseInt( $("input:radio[name=time]:checked").val() );
-    var userAge = parseInt( $("input:radio[name=age]:checked").val() );
-    var quantity = $('#ticketNumber').val();
+    var movieTime = parseInt( $("input:radio[name=time]:checked").val() ); //gives a value to the check radio button
+    var userAge = parseInt( $("input:radio[name=age]:checked").val() ); //gives a value to the check radio button
+    var quantity = $('#ticketNumber').val(); //assigns a value to the number of tickets chosen
 
-    var newPerson = new MovieSpec(featureTitle, movieTime, userAge, quantity);
+    var newPerson = new MovieSpec(featureTitle, movieTime, userAge, quantity);  //assinging constructor instance to the constructor object
 
 
-    $(".receipt").show();
-    $("#filmTitle").text(newPerson.feature);
-    $("#filmTime").text(newPerson.movieTitle() );
-    $("#filmPrice").text(newPerson.price());
+    $(".receipt").show(); //displays the receipt
+    $("#filmTitle").text(newPerson.feature); //calls the prototype and displays the return value to the html
+    $("#filmTime").text(newPerson.movieTitle() ); //calls the prototype and displays the return value to the html
+    $("#filmPrice").text(newPerson.price()); //calls the prototype and displays the return value to the html
 
   });
 });
